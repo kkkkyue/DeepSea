3f59d6508b3fbca086be44b7b70c90e2e914d96b
kkkkyue <junewong@outlook.com>
2023-05-29 02:23:08 +0800

    fix

--- src/gh.py
+++ src/gh.py
@@ -24,7 +24,7 @@ class GH():
         ghLatestRelease = releases[0]
         tag = ghLatestRelease.tag_name
         repo_name = module["repo"].split('/')[1]
-        output_file = open('repo_tags.txt', 'w')
+        output_file = open('./repo_tags.txt', 'w')
         output_file.write(f"{repo_name}: {tag}\n")
         output_file.close()
         for pattern in module["regex"]:
